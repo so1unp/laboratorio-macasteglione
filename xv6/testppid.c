d468315d9439a10dd35dea5f64bbdf700c41c195
macasteglione <matiascasteglione2002@gmail.com>
2024-04-11 13:37:14 -0300

    llamada al sistema pscnt en proceso

---
+++ xv6/testppid.c
@@ -0,0 +1,13 @@
+/*
+ * Invoca la llamada al sistema getppid().
+ */
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int main(void)
+{
+    // Descomentar una vez implementada la llamada al sistema.
+    printf("%d\n", getppid());
+    return 0;
+}
