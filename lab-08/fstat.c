7f85bc88279ccd7ccc3a9138543f283fe2e7ab00
matiascasteglione <matiascasteglione2002@gmail.com>
2024-06-13 18:23:50 -0300

    lab 8 terminado

--- lab-08/fstat.c
+++ lab-08/fstat.c
@@ -1,9 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
 
+void print_file_type(mode_t mode)
+{
+    switch (mode & S_IFMT)
+    {
+    case S_IFREG:
+        printf("Tipo de archivo: regular\n");
+        break;
+
+    case S_IFDIR:
+        printf("Tipo de archivo: directorio\n");
+        break;
+
+    case S_IFCHR:
+        printf("Tipo de archivo: dispositivo de caracteres\n");
+        break;
+
+    case S_IFBLK:
+        printf("Tipo de archivo: dispositivo de bloques\n");
+        break;
+
+    case S_IFIFO:
+        printf("Tipo de archivo: FIFO/pipe\n");
+        break;
+
+    case S_IFLNK:
+        printf("Tipo de archivo: enlace simbólico\n");
+        break;
+
+    case S_IFSOCK:
+        printf("Tipo de archivo: socket\n");
+        break;
+
+    default:
+        printf("Tipo de archivo: desconocido\n");
+        break;
+    }
+}
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+    if (argc != 2)
+    {
+        fprintf(stderr, "Uso: %s <nombre_de_archivo>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    struct stat file_stat;
+    if (stat(argv[1], &file_stat) == -1)
+    {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("ID del usuario propietario: %u\n", file_stat.st_uid);
+    print_file_type(file_stat.st_mode);
+    printf("Número de i-nodo: %lu\n", file_stat.st_ino);
+    printf("Tamaño en bytes: %ld\n", file_stat.st_size);
+    printf("Número de bloques que ocupa en disco: %ld\n", file_stat.st_blocks);
+    printf("Tiempo de última modificación: %s", ctime(&file_stat.st_mtime));
+    printf("Tiempo de último acceso: %s", ctime(&file_stat.st_atime));
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
--- lab-08/write_bytes.c
+++ lab-08/write_bytes.c
@@ -3,14 +3,18 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
+    clock_t inicio = clock();
+
     size_t bufSize, numBytes, thisWrite, totWritten;
     char *buf;
     int sync, fd, openFlags;
 
-    if (argc != 5) {
+    if (argc != 5)
+    {
         fprintf(stderr, "Uso: %s num-bytes buf-size sync archivo\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -20,7 +24,8 @@ int main(int argc, char *argv[])
     sync = atoi(argv[3]);
 
     buf = malloc(bufSize);
-    if (buf == NULL) {
+    if (buf == NULL)
+    {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
@@ -28,27 +33,54 @@ int main(int argc, char *argv[])
     openFlags = O_CREAT | O_WRONLY;
 
     fd = open(argv[4], openFlags, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
+    if (fd == -1)
+    {
         perror("open");
         exit(EXIT_FAILURE);
     }
 
     // Esta condicion esta presente para evitar el warning que dice que sync no esta siendo usada.
     // Se puede eliminar cuando se implemente fsync() y fdatasync()
-    sync = sync == 1 ? 0 : 1;
+    //sync = sync == 1 ? 0 : 1;
 
-    for (totWritten = 0; totWritten < numBytes; totWritten += thisWrite) {
+    switch (sync)
+    {
+    case 0:
+        break;
+
+    case 1:
+        fsync(fd);
+        break;
+
+    case 2:
+        fdatasync(fd);
+        break;
+
+    default:
+        break;
+    }
+
+    for (totWritten = 0; totWritten < numBytes; totWritten += thisWrite)
+    {
         thisWrite = numBytes - totWritten > bufSize ? bufSize : numBytes - totWritten;
 
-        if (write(fd, buf, thisWrite) != thisWrite) {
+        if (write(fd, buf, thisWrite) != thisWrite)
+        {
             perror("write");
             exit(EXIT_FAILURE);
         }
     }
 
-    if (close(fd) == -1) {
+    if (close(fd) == -1)
+    {
         perror("close");
         exit(EXIT_FAILURE);
     }
+
+    clock_t fin = clock();
+
+    float tiempoEjecucion = (float)(fin - inicio) / CLOCKS_PER_SEC;
+    printf("%f\n", tiempoEjecucion);
+
     exit(EXIT_SUCCESS);
 }
